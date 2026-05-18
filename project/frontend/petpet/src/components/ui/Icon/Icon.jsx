b81f162f9ddb525be3191b9ae414fc306de92e11
Peven Dmitriy <dima.peven@yandex.ru>
2026-05-18 23:47:04 +0300

    Обновление иконки
    Теперь можно гибче работать со стилями, обозначая цвет иконки в CSS, а не напрямую

--- project/frontend/petpet/src/components/ui/Icon/Icon.jsx
+++ project/frontend/petpet/src/components/ui/Icon/Icon.jsx
@@ -1,14 +1,24 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import clsx from 'clsx';
 
 export default function Icon(
     {
         icon,
         size = "1x",
-        color = "white",
+        color,
+        className,
+        style,
         ...rest
     }
 ){
+    const combinedStyle = color ? { color, ...style } : style;
+
     return(
-        <FontAwesomeIcon icon={icon} size={size} color={color} {...rest}/>
+        <FontAwesomeIcon 
+            icon={icon} 
+            size={size}
+            className={clsx('icon', className)} // базовый класс + внешние
+            style={combinedStyle} 
+            {...rest}/>
     );
 }
