6144b1cec027546111f41b22e8b454419f8fff21
Peven Dmitriy <dima.peven@yandex.ru>
2026-05-20 02:45:36 +0300

    Обновление профиля
    Теперь есть кнопка создания новой записи (<Button>) и работа с лентой по профилю (<PostFeed>)

--- project/frontend/petpet/src/components/pages/profile/Profile.jsx
+++ project/frontend/petpet/src/components/pages/profile/Profile.jsx
@@ -1,8 +1,16 @@
+import classes from './styles/Profile.module.css';
+
 import UserHeader from '../../ui/UserHeader/UserHeader';
+import Button from '../../ui/Button/Button';
+import PostFeed from '../../ui/PostFeed/PostFeed';
+import Icon from '../../ui/Icon/Icon';
+
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 export default function Profile(){
 
     const userData = {
+        id: 'user_123',
         name: "His Majesty",
         bio: "Мамочка трёх ангелочков Меня сложно найти, легко потерять и невозможно забыть Великолепнейший мужчина этого мира",
         avatar: "/images/user_profile_test/user-avatar.jpg",
@@ -14,9 +22,94 @@ export default function Profile(){
         ],
     };
 
+    const userPosts = [
+    {
+        "id": "post_001",
+        "author": {
+            "avatar": "/images/user_profile_test/user-avatar.jpg",
+            "userName": "His Majesty",
+            "petName": "Котлетка"
+        },
+        "media": [
+                    {
+                        "type": "image",
+                        "url": "images/posts_test/cat.jpg",
+                        "alt": "Котлетка"
+                    },
+                    {
+                        "type": "image",
+                        "url": "images/posts_test/cat.jpg",
+                        "alt": "Котлетка"
+                    }
+        ],
+        "caption": "Моя Котлетка вышла погулять!",
+        "stats": {
+            "likes": 456,
+            "hearts": 15,
+            "comments": 21348,
+            "reposts": 2
+        },
+        "createdAt": "2024-05-19T10:30:00Z",
+        "isOwn": true
+    },
+    {
+        "id": "post_002",
+        "author": {
+            "avatar": "/images/user_profile_test/user-avatar.jpg",
+            "userName": "His Majesty",
+            "petName": "Табурет"
+        },
+        "media": [
+            {
+                "type": "image",
+                "url": "images/posts_test/dog.jpg",
+                "alt": "Табурет спит"
+            },
+            {
+                "type": "image",
+                "url": "images/posts_test/dog.jpg",
+                "alt": "Табурет спит"
+            },
+            {
+                "type": "image",
+                "url": "images/posts_test/dog.jpg",
+                "alt": "Табурет спит"
+            }
+        ],
+        "caption": "",
+        "stats": {
+            "likes": 128,
+            "hearts": 8149,
+            "comments": 24,
+            "reposts": 12
+        },
+        "createdAt": "2024-05-18T18:15:00Z",
+        "isOwn": true
+    }
+]
+
+    const handleCreatePost = () => {
+        console.log('Открыть модалку создания поста');
+        // TODO: открыть модальное окно или перейти на /create-post
+    };
+
     return(
         <>
             <UserHeader userData={userData}/>
+            <div className={classes.profileContainer}>
+                <Button variant="creation" fullWidth={true} onClick={handleCreatePost}>
+                    <Icon icon={faPlus} />
+                    <span>Что у вас нового?</span>
+                </Button>
+        
+                {userPosts.length > 0 ? (
+                    <PostFeed posts={userPosts} />
+                        ) : (
+                    <div className={classes.emptyState}>
+                        <p>У вас пока нет публикаций</p>
+                    </div>
+                )}
+            </div>
         </>
     );
 }
