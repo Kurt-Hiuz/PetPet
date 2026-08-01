1bf8494b1d14e6fcd9f7e2ac32fad6a9ee662312
Peven Dmitriy <dima.peven@yandex.ru>
2026-08-02 01:34:01 +0300

    Обработка отсутсвия src

--- project/frontend/petpet/src/components/ui/Post/PostMedia/renderers/usingRenderers/ImageRenderer.jsx
+++ project/frontend/petpet/src/components/ui/Post/PostMedia/renderers/usingRenderers/ImageRenderer.jsx
@@ -3,7 +3,7 @@ import { IMAGES } from "@config/assetsConfig";
 export default function ImageRenderer({ url, alt, className }) {
     return (
         <img 
-            src={url} 
+            src={url || IMAGES.NO_IMG} 
             alt={alt || IMAGES.NO_ALT}
             className={className}
             loading="lazy"
