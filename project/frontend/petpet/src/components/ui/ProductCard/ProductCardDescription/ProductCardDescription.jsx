822bcaaeb172d729d0ceac32460ce0402a491f1e
Peven Dmitriy <dima.peven@yandex.ru>
2026-08-07 04:14:22 +0300

    Добавление комментариев и выход по !data

--- project/frontend/petpet/src/components/ui/ProductCard/ProductCardDescription/ProductCardDescription.jsx
+++ project/frontend/petpet/src/components/ui/ProductCard/ProductCardDescription/ProductCardDescription.jsx
@@ -7,7 +7,18 @@ import { faStar } from '@fortawesome/free-solid-svg-icons';
 import { formatCount } from '@utils/formatCount';
 import { pluralizeReviews } from '@utils/pluralize';
 
+/**
+ * Описание товара: цена, название, рейтинг, количество отзывов.
+ * @param {Object} props
+ * @param {Object} props.data - Нормализованные данные описания
+ * @param {number} props.data.price
+ * @param {string} props.data.title
+ * @param {number} props.data.stars
+ * @param {number} props.data.reviewCount
+ */
 export default function ProductCardDescription({data}){
+    if (!data) return null;
+    
     return(
         <div className={classes.description}>
             <p className={classes.price}>{data.price} руб.</p>
