b6571519d887c10e0d9471f6b45610c22439a916
Peven Dmitriy <dima.peven@yandex.ru>
2026-08-13 04:31:13 +0300

    Добавление кнопки вызова модального окна

--- project/frontend/petpet/src/components/template/Header/Header.jsx
+++ project/frontend/petpet/src/components/template/Header/Header.jsx
@@ -2,10 +2,13 @@ import classes from './styles/Header.module.css'
 
 import { ThemeSwitcher } from '@ui/ThemeSwitcher/ThemeSwitcher';
 
+import CartButton from '@features/Cart/CartButton/CartButton';
+
 export default function Header() {
     return(
         <header className={classes.header}>
             <ThemeSwitcher/>
+            <CartButton/>
         </header>
     );
 }
